4d22511a8062ba93f5f886224a66561d5084178a
Jacob Zhu <jzhu46@students.cs.ubc.ca>
2024-05-12 13:21:14 -0700

    Finished About page, implemented average background colour and sessionStorage functionality

--- src/scripts/image_lib.js
+++ src/scripts/image_lib.js
@@ -1,44 +1,10 @@
-// Initial team initialization JSON - pictures taken from https://en.wikipedia.org/wiki/Wikipedia:Featured_pictures/Animals/Mammals
-const initialTeam = `{
-    "team members": [
-        {
-            "name": "Iberian Lynx",
-            "description": "A member of one of the four extant species within the wild cat genus Lynx, endemic to the Iberian Peninsula in southwestern Europe.",
-            "age": 20,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg/1024px-Lince_ib%C3%A9rico_%28Lynx_pardinus%29%2C_Almuradiel%2C_Ciudad_Real%2C_Espa%C3%B1a%2C_2021-12-19%2C_DD_07.jpg"
-        },
-        {
-            "name": "Black-tailed Prairie Dog",
-            "description": "A rodent of the family Sciuridae found in the Great Plains of North America. Prior to habitat destruction, its species may have been the most abundant prairie dog in central North America.",
-            "age": 5,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Cynomys_ludovicianus_GNP_21.jpg/1024px-Cynomys_ludovicianus_GNP_21.jpg"
-        },
-        {
-            "name": "Red Panda",
-            "description": "A small mammal native to the eastern Himalayas and southwestern China, well adapted to climbing due to its flexible joints and curved semi-retractile claws.",
-            "age": 8,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Red_Panda_%2824986761703%29.jpg"
-        },
-        {
-            "name": "Geoffroy's Tamarin",
-            "description": "A type of small monkey, found in Panama and Colombia, which eats a variety of foods, including insects, plant exudates, fruits, and other plant parts.",
-            "age": 14,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/d/df/Geoffroy%27s_tamarin_%28Saguinus_geoffroyi%29_2.jpg"
-        },
-        {
-            "name": "European Hedgehog",
-            "description": "A member of a hedgehog species native to Europe, and a favourite in European gardens, both for its endearing appearance and its preference for eating a range of garden pests.",
-            "age": 3,
-            "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Erinaceus_europaeus_%28Linnaeus%2C_1758%29.jpg"
-        }
-    ]
-}`;
-
-let team = JSON.parse(initialTeam)["team members"];
-
+// TODO: move the setBackgroundColour call to an onload method, and store the average colour somehow
 // Showing the list of team members in a given container div, given the div id
 function showTeamMembers(id) {
+    const team = JSON.parse(sessionStorage.getItem("team"))["team members"];
     const container = document.getElementById(id);
+
+    // Short-circuiting if you press "Show" multiple times in a row
     if (container.innerHTML !== "") {
         return;
     }
@@ -48,6 +14,7 @@ function showTeamMembers(id) {
         toAdd += "<p>No members in team!</p>"
     } else {
         for (const member of team) {
+            // let r = member.avgColour.r, g = member.avgColour.g, b = member.avgColour.b;
             toAdd += '<div class="team_card">';
             toAdd += member.name + "<br><br>";
             toAdd += member.description + "<br><br>";
@@ -61,7 +28,13 @@ function showTeamMembers(id) {
     container.innerHTML = toAdd;
 }
 
-// Given an image url, sets the colour of the corresponding card to the average colour of the image
+// Hiding the list of team members in a given container div, given the div id
+function hideTeamMembers(id) {
+    const container = document.getElementById(id);
+    container.innerHTML = "";
+}
+
+// Given an image url, sets the avgColour field of the corresponding member object in sessionStorage
 function setBackgroundColour(imageURL) {
     let image = new Image();
     image.src = imageURL;
@@ -117,10 +90,4 @@ function getTextColour(r, g, b) {
     } else {
         return "white";
     }
-}
-
-// Hiding the list of team members in a given container div, given the div id
-function hideTeamMembers(id) {
-    const container = document.getElementById(id);
-    container.innerHTML = "";
 }
