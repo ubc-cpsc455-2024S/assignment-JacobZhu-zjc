7750a8cedf2259bafee7ff79fe240312493718eb
Jacob Zhu <jzhu46@students.cs.ubc.ca>
2024-06-03 07:35:23 -0700

    Added documentation

--- team-manager/src/redux/store.js
+++ team-manager/src/redux/store.js
@@ -1,3 +1,4 @@
+// Code and explanations of Redux attributed to ChatGPT
 import {createStore} from "redux";
 import rootReducer from "./reducers";
 import {composeWithDevTools} from "@redux-devtools/extension";
